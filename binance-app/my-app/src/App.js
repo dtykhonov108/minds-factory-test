0ad1b4cf9693546bd33404a1be4e5d6d470130b9
Dmytro Tykhonov <dtykhonov108@gmail.com>
2022-11-14 11:33:46 +0200

    updated binance app

--- binance-app/my-app/src/App.js
+++ binance-app/my-app/src/App.js
@@ -2,19 +2,21 @@ import './App.css';
 import styled from 'styled-components';
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
-
+//import Qs from 'qs';
+//import crypto from 'crypto';
 
 const DivMain = styled.div`
 background-color: #282c34;
 display: flex;
-justify-content: space-evenly;
 width:100%;
-height:100vh;
+height:100%;
+align-items: flex-start;
+flex-wrap: wrap;
 `;
 
 const DivBinance = styled.div`
 margin-top:20px;
-height:60vh;
+
 width:30%;
 color: white;
 text-align:center;
@@ -24,7 +26,7 @@ font-family:sans-serif;
 
 const DivKuna = styled.div`
 margin-top:20px;
-height:60vh;
+
 width:30%;
 color: white;
 text-align:center;
@@ -32,6 +34,12 @@ font-size:20px;
 font-family:sans-serif;
 `;
 
+const DivKunaBin = styled.div`
+display: flex;
+justify-content: space-evenly;
+width:100%;
+height:100%;
+`
 
 const Greet = styled.div`
 text-align:left;
@@ -72,13 +80,76 @@ text-align:left;
 width:50%;
 `;
 
-
+const DivItemTwo = styled.div`
+text-align:left;
+width:50%;
+color:white;
+`;
 
 
 const Ul = styled.ul`
 margin-left:10px;
 margin-right:10px;
 padding-left:0px;
+`;
+
+const DivForm = styled.div`
+width:100%;
+display:flex;
+justify-content: center;
+`;
+const Form = styled.form`
+margin-top: 100px;
+width:30%;
+height:500px;
+`;
+
+const Input = styled.input`
+width:350px;
+height:30px;
+font-size:20px;
+font-family:sans-serif;
+border-radius:5px;
+border: black 2px solid;
+margin-top:3px;
+`;
+
+const ButtonDiv = styled.div`
+margin-top:10px;
+width:70%;
+display:flex;
+justify-content: space-between;
+`;
+
+
+const Buy = styled.button`
+width:130px;
+height:40px;
+background-color: mediumseagreen;
+color:white;
+font-size:20px;
+font-family:sans-serif;
+border-radius:5px;
+border: black 2px solid;
+
+&:hover{
+  cursor:pointer;
+}
+`;
+
+const Sell = styled.button`
+width:130px;
+height:40px;
+background-color: red;
+color:white;
+font-size:20px;
+font-family:sans-serif;
+border-radius:5px;
+border: black 2px solid;
+
+&:hover{
+  cursor:pointer;
+}
 `;
 
 // <div>{percentBin}</div>
@@ -110,14 +181,12 @@ useEffect( () => {
    return () =>{clearTimeout(timer)}
 }, [])
 
-const percentBin = Math.round((kunaBids[0][0]-binanceAsks[9][0])/kunaBids[0][0]*100*100)/100;
-const percentBinTwo = Math.round(((kunaBids[0][0]-binanceBids[0][0])/kunaBids[0][0])*100*100)/100;
-//const percentBin2 = (binanceBids[1][0]-kunaAsks[9][0])/kunaAsks[9][0]*100
+//const percentBin = Math.round((kunaBids[0][0]-binanceAsks[9][0])/kunaBids[0][0]*100*100)/100;
+//const percentBinTwo = Math.round(((kunaBids[0][0]-binanceBids[0][0])/kunaBids[0][0])*100*100)/100;
 
 
-//const percentKuna = Math.round((kunaBids[1][0] - Math.round(binanceBids[9][0]*100)/100)/binanceBids[9][0]*100*100)/100;
-const percentKuna = Math.round((binanceBids[0][0]-kunaAsks[9][0])/binanceBids[0][0]*100*100)/100;
-const percentKunaTwo = Math.round(((binanceAsks[9][0]-kunaAsks[9][0])/binanceAsks[9][0])*100*100)/100;
+//const percentKuna = Math.round((binanceBids[0][0]-kunaAsks[9][0])/binanceBids[0][0]*100*100)/100;
+//const percentKunaTwo = Math.round(((binanceAsks[9][0]-kunaAsks[9][0])/binanceAsks[9][0])*100*100)/100;
 //
 /*
   const getBinance = async () =>{
@@ -155,9 +224,13 @@ const percentKunaTwo = Math.round(((binanceAsks[9][0]-kunaAsks[9][0])/binanceAsk
 
  } ;
 */
-  return (
-    <DivMain>
 
+
+ 
+  return (
+      
+    <DivMain>
+      <DivKunaBin>
       <DivBinance>
 
         <Greet>
@@ -166,7 +239,7 @@ const percentKunaTwo = Math.round(((binanceAsks[9][0]-kunaAsks[9][0])/binanceAsk
         <Ul>{
              binanceAsks.map((item)=>{
               return(
-               <LiAsks><DivItem>{item[0]}</DivItem><DivItem>{item[1]}</DivItem></LiAsks>     
+               <LiAsks><DivItem>{item[0]}</DivItem><DivItemTwo>{item[1]}</DivItemTwo></LiAsks>     
                  );
               }) 
                
@@ -174,16 +247,17 @@ const percentKunaTwo = Math.round(((binanceAsks[9][0]-kunaAsks[9][0])/binanceAsk
         <Ul>{
              binanceBids.map((item)=>{
               return(
-               <LiBids><DivItem>{item[0]}</DivItem><DivItem>{item[1]}</DivItem></LiBids>     
+               <LiBids><DivItem>{item[0]}</DivItem><DivItemTwo>{item[1]}</DivItemTwo></LiBids>     
                  );
               }) 
               
         }</Ul>
-           
-           <div>{percentBin}</div>
-           <div>{percentBinTwo}</div>   
-          
-
+        <> { binanceBids.length > 0 && kunaBids.length > 0  && binanceAsks.length > 0  &&<>
+          <div>{Math.round((kunaBids[0][0]-binanceAsks[9][0])/kunaBids[0][0]*100*100)/100}</div>
+          <div>{Math.round(((kunaBids[0][0]-binanceBids[0][0])/kunaBids[0][0])*100*100)/100}</div>
+         </>
+          }</>
+            
       </DivBinance>
 
       <DivKuna>
@@ -194,7 +268,7 @@ const percentKunaTwo = Math.round(((binanceAsks[9][0]-kunaAsks[9][0])/binanceAsk
         <Ul>{
              kunaAsks.map((item)=>{
               return(
-               <LiAsks><DivItem>{item[0]}</DivItem><DivItem>{-item[1]}</DivItem></LiAsks>     
+               <LiAsks><DivItem>{item[0]}</DivItem><DivItemTwo>{-item[1]}</DivItemTwo></LiAsks>     
                  );
               }) 
                
@@ -204,18 +278,41 @@ const percentKunaTwo = Math.round(((binanceAsks[9][0]-kunaAsks[9][0])/binanceAsk
         <Ul>{
              kunaBids.map((item)=>{
               return(
-               <LiBids><DivItem>{item[0]}</DivItem><DivItem>{item[1]}</DivItem></LiBids>     
+               <LiBids><DivItem>{item[0]}</DivItem><DivItemTwo>{item[1]}</DivItemTwo></LiBids>     
                  );
               }) 
   
         }</Ul>
-     
-     <div>{percentKuna}</div>
-      <div>{percentKunaTwo}</div>
+       <> { binanceBids.length > 0 && kunaAsks.length > 0  && binanceAsks.length > 0  &&<>
+        <div>{Math.round((binanceBids[0][0]-kunaAsks[9][0])/binanceBids[0][0]*100*100)/100}</div> 
+        <div>{Math.round(((binanceAsks[9][0]-kunaAsks[9][0])/binanceAsks[9][0])*100*100)/100}</div>
+        </> }</>
       </DivKuna>
+        </DivKunaBin>
+
+
+      <DivForm>
+
+        <Form>
+          <Input placeholder='Price'></Input>
+          <Input placeholder="Amount"></Input>
+          <Input placeholder='Value'></Input>
+
+          <ButtonDiv>
+            <Buy>buy</Buy>
+            <Sell>sell</Sell>
+          </ButtonDiv>
+
+        </Form>
+
+      </DivForm>
+
 
     </DivMain>
+      
+
   );
 }
 
 export default App;
+
